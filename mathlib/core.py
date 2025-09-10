f8d8392dc9c7fe342237c77f0245b15e9b32aad8
Adrián Flores <flo21500@uvg.edu.gt>
2025-09-10 15:39:35 -0600

    chore: break is prime

--- mathlib/core.py
+++ mathlib/core.py
@@ -60,18 +60,18 @@ def is_prime(n: int) -> bool:
     bool
         True if n is prime, False otherwise.
     """
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
+#    if n <= 1:
+ #       return False
+  #  if n <= 3:
+   #     return True
+  #  if n % 2 == 0 or n % 3 == 0:
+   #     return False
 
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
+    #i = 5
+    #while i * i <= n:
+    #    if n % i == 0 or n % (i + 2) == 0:
+    #        return False
+    #    i += 6
     return True
 
 
